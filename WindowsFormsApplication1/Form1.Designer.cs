193f32e7630678043ecb598369e610b10d835ed6
ywj1991 <470578575@qq.com>
2018-12-14 09:48:58 +0800

    20181214

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,37 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.cmdSave = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmdRead = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Location = new System.Drawing.Point(82, 43);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(721, 327);
-            this.textBox1.TabIndex = 0;
+            this.txtName.Location = new System.Drawing.Point(82, 43);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(139, 21);
+            this.txtName.TabIndex = 0;
             // 
-            // button1
+            // cmdSave
             // 
-            this.button1.Location = new System.Drawing.Point(561, 416);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.cmdSave.Location = new System.Drawing.Point(146, 127);
+            this.cmdSave.Name = "cmdSave";
+            this.cmdSave.Size = new System.Drawing.Size(75, 23);
+            this.cmdSave.TabIndex = 1;
+            this.cmdSave.Text = "保    存";
+            this.cmdSave.UseVisualStyleBackColor = true;
+            this.cmdSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(82, 83);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(139, 21);
+            this.txtNumber.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "姓名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "代码";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 200);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(514, 274);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // cmdRead
+            // 
+            this.cmdRead.Location = new System.Drawing.Point(146, 171);
+            this.cmdRead.Name = "cmdRead";
+            this.cmdRead.Size = new System.Drawing.Size(75, 23);
+            this.cmdRead.TabIndex = 6;
+            this.cmdRead.Text = "读    取";
+            this.cmdRead.UseVisualStyleBackColor = true;
+            this.cmdRead.Click += new System.EventHandler(this.cmdRead_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(935, 513);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(956, 495);
+            this.Controls.Add(this.cmdRead);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.cmdSave);
+            this.Controls.Add(this.txtName);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,8 +122,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button cmdSave;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button cmdRead;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,79 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("33333");
+            Save();
+        }
+        private  void Save()
+        { 
+            string sName = txtName.Text;
+            string sNumber = txtNumber.Text;
+            if (sName == "")
+            {
+                MessageBox.Show("姓名不能为空");
+                return;
+            }
+            if (sNumber == "")
+            {
+                MessageBox.Show("代码不能为空");
+                return;
+            }
+            string sSql = "insert into Emp(Name,Number) values ('" + sName + "','" + sNumber + "')";
+            DbOperation Db = new DbOperation();
+            Db.sqlcmd(sSql);
+            MessageBox.Show("成功");
+
+        }
+        public class DbConnection
+        {
+            public static string sqlcon = "Data Source =DESKTOP-JL0B035\\SQL2008;Initial Catalog=222;Integrated Security=True;User ID=sa;Password=";
+            public SqlConnection getcon()
+            {
+                SqlConnection mycon = new SqlConnection(sqlcon);
+                return mycon;
+            }
+        }
+        
+        class DbOperation
+        {
+            DbConnection constr = new DbConnection();
+            public void sqlcmd(string sqlstr)
+            {
+                try
+                {
+                    SqlConnection conn = constr.getcon();
+                    conn.Open();
+                    SqlCommand scmd = new SqlCommand(sqlstr, conn);
+                    scmd.ExecuteNonQuery();
+                    conn.Dispose();
+                    conn.Close();
+                }
+                catch
+                { }
+            }
+            public DataTable getTable(string sqlstr)
+            {
+                DataTable dt = new DataTable();
+                try
+                {
+                    SqlConnection conn = constr.getcon();
+                    conn.Open();
+                    SqlDataAdapter sda = new SqlDataAdapter(sqlstr, conn);
+                    sda.Fill(dt);
+                    conn.Dispose();
+                    conn.Close();
+                }
+                catch { }
+                return dt;
+            }
+        }
+
+        private void cmdRead_Click(object sender, EventArgs e)
+        {
+            string sSql = "select * from Emp";
+
+            DbOperation Db = new DbOperation();
+            dataGridView1.DataSource= Db.getTable(sSql);
         }
     }
+
 }
